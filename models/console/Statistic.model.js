ca2cd5c8278cd6073ad0e340f550310fba1559cb
K. Nattawut <kitiwan_n@silpakorn.edu>
2019-02-27 15:45:16 +0700

    Update

--- models/console/Statistic.model.js
+++ models/console/Statistic.model.js
@@ -6,7 +6,7 @@ const Statistic = {
       "SELECT " +
       "(SELECT COUNT(`workId`) FROM Works ) AS `w_all`," +
       "(SELECT COUNT(`workId`) FROM Works WHERE `workStatus` > 2 AND `workStatus` < 5 ) AS `w_enabled`," +
-      "(SELECT COUNT(*) FROM RequestWork WHERE `rwStatus` = 1) AS `w_request`, " +
+      "(SELECT COUNT(S2.*) FROM RequestWork S2 JOIN Works S1 ON S1.`workId` = S2.`rwWorkId` WHERE S2.`rwStatus` = 1 AND S1.`workStatus` > 2) AS `w_request`, " +
       "(SELECT SUM(`maxVolume`) FROM Manufacture WHERE `mfStatus` < 4) AS `unit_process`," +
       "(SELECT SUM(`workVolume`) FROM Works WHERE `workStatus` > 2 AND `workStatus` < 5) AS `unit_enabled`," +
       "(SELECT SUM(S2.`rwVolume`) FROM RequestWork S2 JOIN Works S1 ON S1.`workId` = S2.`rwWorkId` WHERE S2.`rwStatus` = 2 AND S1.`workStatus` > 2 AND S1.`workStatus` < 5) AS `unit_approved`," +
